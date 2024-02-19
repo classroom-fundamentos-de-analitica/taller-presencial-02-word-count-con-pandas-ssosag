6896e70d5825dcf34d7327a45658a2532a970c36
ssosag <ssosag@unal.edu.co>
2024-02-19 15:30:09 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -13,20 +13,42 @@ def load_input(input_directory):
     # entrada en el DataFrame.
     #
 
+    filenames = glob.glob(input_directory + "/*.*")
+    dataframes = [
+        pd.read_csv(filename, sep="\t", names=["text"]) for filename in filenames
+    ]
+
+    dataframe = pd.concat(dataframes).reset_index(drop=True)
+    print(dataframe)
+    return dataframe
+
 
 def clean_text(dataframe):
     """Text cleaning"""
     #
     # Elimine la puntuación y convierta el texto a minúsculas.
     #
+    dataframe = dataframe.copy()
+    dataframe["text"] = dataframe["text"].str.lower()
+    dataframe["text"] = dataframe["text"].str.replace(",", "").str.replace(".", "")
+    return dataframe
 
 
 def count_words(dataframe):
     """Word count"""
 
+    dataframe = dataframe.copy()
+    dataframe["text"] = dataframe["text"].str.split()
+    dataframe = dataframe.explode("text").reset_index(drop=True)
+    dataframe["value"] = 1
+    conteo = dataframe.groupby(["text"]).agg({"value": "sum"})
+    dataframe = dataframe.rename(columns={"text": "word"})
+    return conteo
+
 
 def save_output(dataframe, output_filename):
     """Save output to a file."""
+    dataframe.to_csv(output_filename, sep="\t", header=False)
 
 
 #
@@ -34,6 +56,10 @@ def save_output(dataframe, output_filename):
 #
 def run(input_directory, output_filename):
     """Call all functions."""
+    dataframe = load_input(input_directory)
+    dataframe = clean_text(dataframe)
+    dataframe = count_words(dataframe)
+    save_output(dataframe, output_filename)
 
 
 if __name__ == "__main__":
